fbf722b01437e0d15e5a2f120c96025448b4b5cd
aditx <klikadsene@gmail.com>
2017-10-19 16:27:28 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,150 @@
+<html>
+  <head>
+    <title>Beranda</title>
+    <link href="addons/bootstrap.min.css" rel="stylesheet">
+    <link href="css/skin.css" rel="stylesheet"/>
+    <script src="addons/jquery.min.js"></script>
+    <script src="addons/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="index.php">Sistem Rumah Sakit</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="index.php?page=about-us">Visi & Misi</a></li>
+          <li><a href="index.php?page=contact-us">Hubungi Kami</a></li>
+        </ul>
+
+    	</div>
+    </nav>
+
+      <!-- Page Content -->
+      <div class="container">
+
+       <!-- Heading Row -->
+       <div class="row">
+        <div class="col-md-12">
+         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner" role="listbox">
+          <div class="item active">
+           <img src="img/header-3.jpg" alt="...">
+           <div class="carousel-caption">
+           <h3>Kolom 1</h3>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur</p>
+           </div>
+          </div>
+          <div class="item">
+           <img src="img/header-1.jpg" alt="...">
+           <div class="carousel-caption">
+           <h3>Kolom 2</h3>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur</p>
+           </div>
+          </div>
+          <div class="item">
+           <img src="img/header-2.jpg" alt="...">
+           <div class="carousel-caption">
+           <h3>Kolom 3</h3>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur</p>
+           </div>
+          </div>
+          </div>
+
+          <!-- Controls -->
+          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+          </a>
+         </div>
+        </div>
+       </div>
+       <!-- /.row -->
+
+       <div class="row">
+        <div class="col-md-12">
+        <!-- Hello Word yang nyambung-->
+
+         <div class="jumbotron">
+         <h1>Selamat Datang di situs kami</h1>
+         <table>
+          <tr>
+            <td><img src="img/mercubuana.png" width="200" height="150"/></td>
+            <td>
+              <p style="margin-left: 20px;">Sistem rumah sakit ini merupakan sistem yang menampilkan informasi rumah sakit</p>
+              <a class="btn btn-default" href="#" style="margin-left: 20px;">More Info</a>
+            </td>
+          </tr>
+         </table>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+              <div class="article-post">
+                <div class="body-article">
+
+                </div>
+              </div>
+            </div>
+        </div>
+        <!-- /.col-md-4 -->
+       <!-- Content Row -->
+       <div class="row">
+       <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+        <div class="caption">
+          <h3>Other Links</h3>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur</p>
+         </div>
+         </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+        <div class="caption">
+          <h3>Kritik & Saran</h3>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur</p>
+         </div>
+         </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+        <div class="caption">
+          <h3>Temukan kami</h3>
+         <p>Kampus Meruya</p>
+        <p>Jl. Meruya Selatan, Kebon Jeruk, Jakarta Barat</p>
+        <p>T : 021 5840 816 ( Hunting), ext: 2751</p>
+        <p>F : 021 5840 015</p>
+         </div>
+         </div>
+        </div>
+
+       </div>
+        <!-- /.col-md-4 -->
+       </div>
+
+       <footer style="margin-top:20px;">
+        <div class="row">
+         <div class="col-lg-12">
+          <center><p>Copyright &copy; 2017 - Kelompok 8</p></center>
+         </div>
+        </div>
+       </footer>
+
+      </div>
+  </body>
+</html>
